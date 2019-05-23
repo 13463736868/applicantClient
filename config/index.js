7d5902866bb022073eb38ec1fc5b42be4f38eeaf
wangzeqing <wangzeqing@youzhengkeji.com>
2019-05-23 15:15:02 +0800

    '22'

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://192.168.1.173:8085',
+        target: 'http://192.168.1.173:8060',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
