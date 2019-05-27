67cdf15dfab27f06262bd466d201330139193db0
wangzeqing <wangzeqing@youzhengkeji.com>
2019-05-27 18:09:08 +0800

    '5-27-01'

--- src/page/contrace/contrace.vue
+++ src/page/contrace/contrace.vue
@@ -31,24 +31,57 @@
           <p><span class="_span">*</span><b>合作公司：</b></p>
         </Col>
         <Col span="16">
-          <Select v-model="arbiId" @on-change="resChangeArbi('change')">
-            <Option v-for="item in arbiList" :value="item.id" :key="item.id">{{ item.orgName }}</Option>
+          <Select v-model="arbiId" filterable @on-query-change="resChangeQuer">
+            <Option v-for="item in arbiListFilter" :value="item.id" :key="item.id">{{ item.orgName }}</Option>
           </Select>
         </Col>
       </Row>
-      <Row class="_labelFor" v-if="arbiId !== null">
+      <Row class="_labelFor">
         <Col span="6" offset="1">
-          <p><span class="_span">*</span><b>公司旗下人员：</b></p>
+          <p><span class="_span">*</span><b>合同类型：</b></p>
         </Col>
         <Col span="16">
-          <Select v-model="compUserId">
-            <Option v-for="item in compUserList" :value="item.id" :key="item.id">{{ item.name }}</Option>
+          <Select v-model="caseTypeId">
+            <Option :disabled="item.status === 2" v-for="item in caseTypeList" :value="item.value" :key="item.value">{{ item.label }}</Option>
           </Select>
         </Col>
       </Row>
+      <Row class="_labelFor">
+        <Col span="6" offset="1">
+          <p><span class="_span">*</span><b>有效期：</b></p>
+        </Col>
+        <Col span="16">
+          <Select v-model="longTerm">
+            <Option :value="1" :key="1">长期</Option>
+            <Option :value="2" :key="2">时间段</Option>
+          </Select>
+        </Col>
+      </Row>
+      <template v-if="longTerm === 2">
+        <Row class="_labelFor">
+          <Col span="6" offset="1">
+            <p><span class="_span">*</span><b>开始日期：</b></p>
+          </Col>
+          <Col span="16">
+            <DatePicker class="wmax" @on-change="changeDateStart" type="date" confirm placeholder="请选择开始日期"></DatePicker>
+          </Col>
+        </Row>
+        <Row class="_labelFor">
+          <Col span="6" offset="1">
+            <p><span class="_span">*</span><b>结束日期：</b></p>
+          </Col>
+          <Col span="16">
+            <DatePicker class="wmax" @on-change="changeDateEnd" type="date" confirm placeholder="请选择结束日期"></DatePicker>
+          </Col>
+        </Row>
+      </template>
+    </alert-btn-info>
+    <alert-btn-info :alertShow="alertShow.state" @alertCancel="alertCanc('state')" @alertConfirm="stateSave" alertTitle="提示">
+      <p v-if="alertShow.stateN === 1">确定要启用吗？</p>
+      <p v-if="alertShow.stateN === 2">确定要停用吗？</p>
     </alert-btn-info>
     <alert-btn-info :alertShow="alertShow.del" @alertCancel="alertCanc('del')" @alertConfirm="delSave" alertTitle="提示">
-      <p>确定要删除这个合作关系吗</p>
+      <p>确定要删除这个合作关系吗？</p>
     </alert-btn-info>
   </div>
 </template>
@@ -74,14 +107,28 @@ export default {
             align: 'center'
           },
           {
-            title: '接收员工',
-            key: 'mauserName',
+            title: '接收管理员',
+            key: 'masterName',
             align: 'center'
           },
           {
             title: '创建时间',
             key: 'createTime',
             align: 'center'
+          },
+          {
+            title: '合同类型',
+            key: 'casetypeName',
+            align: 'center'
+          },
+          {
+            title: '有效期',
+            key: 'longTerm',
+            align: 'center',
+            render: (h, params) => {
+              return h('span', {
+              }, params.row.longTerm === 1 ? '长期' : (params.row.longTerm === 2 ? params.row.beginTime.substr(0, 10) + ' ~ ' + params.row.endTime.substr(0, 10) : '未知状态'))
+            }
           },
           {
             title: '审核状态',
@@ -127,12 +174,26 @@ export default {
                       type: 'primary',
                       size: 'small'
                     },
+                    style: {
+                      marginRight: '5px'
+                    },
                     on: {
                       click: () => {
                         this.resDel(params.index)
                       }
                     }
-                  }, '删除')
+                  }, '删除'),
+                  h('Button', {
+                    props: {
+                      type: 'primary',
+                      size: 'small'
+                    },
+                    on: {
+                      click: () => {
+                        this.resState(params.index)
+                      }
+                    }
+                  }, params.row.state === 1 ? '停用' : (params.row.state === 2 ? '启用' : '未知'))
                 ])
               } else {
                 return h('div', [
@@ -146,10 +207,21 @@ export default {
                     },
                     on: {
                       click: () => {
-                        this.resDel(params.row.filepath)
+                        this.resDel(params.index)
                       }
                     }
-                  }, '删除')
+                  }, '删除'),
+                  h('Button', {
+                    props: {
+                      type: 'primary',
+                      size: 'small'
+                    },
+                    on: {
+                      click: () => {
+                        this.resState(params.index)
+                      }
+                    }
+                  }, params.row.state === 1 ? '停用' : (params.row.state === 2 ? '启用' : '未知'))
                 ])
               }
             }
@@ -164,12 +236,18 @@ export default {
       },
       arbiId: null,
       arbiList: [],
-      compUserId: null,
-      compUserList: [],
+      arbiListFilter: [],
+      caseTypeId: null,
+      caseTypeList: [],
+      longTerm: null,
+      beginTime: '',
+      endTime: '',
       alertShow: {
         del: false,
         id: null,
-        add: false
+        add: false,
+        state: false,
+        stateN: null
       }
     }
   },
@@ -203,32 +281,47 @@ export default {
       this.pageObj.pageNum = page
       this.resPayList()
     },
-    resChangeArbi (type) {
-      if (type === 'change') {
-        this.compUserId = null
+    resChangeQuer (querys) {
+      let query = querys.replace(/(^\s*)|(\s*$)/g, '')
+      if (query === '') {
+        this.arbiListFilter = []
+      } else {
+        this.arbiListFilter = this.arbiList.filter(a => a.orgName === query)
       }
-      axios.post('/MaCustomerTypeUserController/queryMauser', {
-        id: this.arbiId
-      }).then(res => {
-        this.compUserList = res.data.data
+    },
+    changeDateStart (val) {
+      this.beginTime = val
+    },
+    changeDateEnd (val) {
+      this.endTime = val
+    },
+    resEdit (index) {
+      let _obj = this.payList.bodyList[index]
+      this.arbiId = _obj.orgId
+      this.caseTypeId = _obj.casetypeId
+      this.longTerm = _obj.longTerm
+      this.alertShow.id = _obj.id
+      this.resAdd()
+    },
+    resAdd () {
+      axios.post('/caseType/list').then(res => {
+        let _dataList = res.data.data
+        this.caseTypeList = _dataList.map((a, b) => {
+          let _o = {}
+          _o.value = a.id
+          _o.label = a.caseTypeName
+          _o.status = a.status
+          return _o
+        })
       }).catch(e => {
         this.$Message.error({
           content: '错误信息:' + e + ' 稍后再试',
           duration: 5
         })
       })
-    },
-    resEdit (index) {
-      let _obj = this.payList.bodyList[index]
-      this.arbiId = _obj.orgid
-      this.compUserId = _obj.mauserId
-      this.alertShow.id = _obj.id
-      this.resAdd()
-      this.resChangeArbi('edit')
-    },
-    resAdd () {
       axios.post('/MaCustomerTypeUserController/orglist').then(res => {
         this.arbiList = res.data.data
+        this.arbiListFilter = this.arbiList.filter(a => a.id === this.arbiId)
       }).catch(e => {
         this.$Message.error({
           content: '错误信息:' + e + ' 稍后再试',
@@ -244,35 +337,96 @@ export default {
           duration: 5
         })
         return false
-      } else if (this.compUserId === null || this.compUserId === undefined) {
+      } else if (this.caseTypeId === null || this.caseTypeId === undefined) {
         this.$Message.warning({
-          content: '请先选择公司旗下人员',
+          content: '请先选择一个合同类型',
           duration: 5
         })
         return false
-      } else {
-        let _data = {
-          orgid: this.arbiId,
-          mauserid: this.compUserId
-        }
-        if (this.alertShow.id !== null) {
-          _data.id = this.alertShow.id
-        }
-        this.alertShow.add = false
-        axios.post('/MaCustomerTypeUserController/add/' + (this.alertShow.id === null ? '1' : '2'), _data).then(res => {
-          this.resSearch()
-          this.alertCanc('add')
-          this.$Message.success({
-            content: '操作成功',
-            duration: 2
-          })
-        }).catch(e => {
-          this.$Message.error({
-            content: '错误信息:' + e + ' 稍后再试',
+      } else if (this.longTerm === null) {
+        this.$Message.warning({
+          content: '请先选择一个有效期',
+          duration: 5
+        })
+        return false
+      } else if (this.longTerm === 2) {
+        if (this.beginTime === '') {
+          this.$Message.warning({
+            content: '请先选择开始日期',
             duration: 5
           })
-        })
+        } else if (this.endTime === '') {
+          this.$Message.warning({
+            content: '请先选择结束日期',
+            duration: 5
+          })
+        } else {
+          let beginT = this.beginTime.substr(0, 10).split('-').join('')
+          let endT = this.endTime.substr(0, 10).split('-').join('')
+          if (endT - beginT < 0) {
+            this.$Message.warning({
+              content: '结束时间不能小于开始时间',
+              duration: 5
+            })
+          } else {
+            this.addSaves()
+          }
+        }
+      } else {
+        this.addSaves()
       }
+    },
+    addSaves () {
+      let _data = {
+        orgId: this.arbiId,
+        casetypeId: this.caseTypeId,
+        longTerm: this.longTerm
+      }
+      if (this.alertShow.id !== null) {
+        _data.id = this.alertShow.id
+      }
+      if (this.longTerm === 2) {
+        _data.beginTime = this.beginTime + ' 00:00:00'
+        _data.endTime = this.endTime + ' 23:59:59'
+      }
+      this.alertShow.add = false
+      axios.post('/MaCustomerTypeUserController/add/' + (this.alertShow.id === null ? '1' : '2'), _data).then(res => {
+        this.resSearch()
+        this.alertCanc('add')
+        this.$Message.success({
+          content: '操作成功',
+          duration: 2
+        })
+      }).catch(e => {
+        this.alertCanc('add')
+        this.$Message.error({
+          content: '错误信息:' + e + ' 稍后再试',
+          duration: 5
+        })
+      })
+    },
+    resState (index) {
+      this.alertShow.id = this.payList.bodyList[index].id
+      this.alertShow.stateN = this.payList.bodyList[index].state === 1 ? 2 : 1
+      this.alertShow.state = true
+    },
+    stateSave () {
+      axios.post('/MaCustomerTypeUserController/updateState', {
+        id: this.alertShow.id,
+        state: this.alertShow.stateN
+      }).then(res => {
+        this.resSearch()
+        this.alertCanc('state')
+        this.$Message.success({
+          content: '操作成功',
+          duration: 2
+        })
+      }).catch(e => {
+        this.$Message.error({
+          content: '错误信息:' + e + ' 稍后再试',
+          duration: 5
+        })
+      })
     },
     resDel (index) {
       this.alertShow.id = this.payList.bodyList[index].id
@@ -298,6 +452,11 @@ export default {
     },
     alertCanc (type) {
       switch (type) {
+        case 'state':
+          this.alertShow.id = null
+          this.alertShow.stateN = null
+          this.alertShow.state = false
+          break
         case 'del':
           this.alertShow.id = null
           this.alertShow.del = false
@@ -305,9 +464,11 @@ export default {
         case 'add':
           this.alertShow.add = false
           this.arbiId = null
-          this.compUserId = null
+          this.caseTypeId = null
           this.arbiList = []
-          this.compUserList = []
+          this.arbiListFilter = []
+          this.caseTypeList = []
+          this.longTerm = null
           this.alertShow.id = null
           break
       }
